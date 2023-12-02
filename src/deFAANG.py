a6eea9bdb25c01d73df2a47b2be5e644a52d0795
tjcanro <79056896+tjcanro@users.noreply.github.com>
2023-12-01 21:56:55 -0500

    added the functionality of the program returning you the list of optimal amounts to spend on each stock

--- src/deFAANG.py
+++ src/deFAANG.py
@@ -2,6 +2,8 @@ import os
 import heapq
 import pickle
 import numpy as np
+import pandas as pd
+
 
 class MaxHeap:
     def __init__(self, max_size):
@@ -32,6 +34,7 @@ class MaxHeap:
         print("Values in the max-heap:")
         for value, name in self.heap:
             print(f"Name: {name}, Value: {-value}")
+
 
 class MinHeap:
     def __init__(self, max_size):
@@ -64,31 +67,65 @@ class MinHeap:
             print(f"Name: {name}, Value: {value}")
 
 
+def amount_to_invest(stocks, risk, budget):
+    # normalize the value of standard deviations for each of the stocks
+    print("Your optimal portfolio: ")
+    total = 0.0
+    stock_dict = {}
+
+    # Use items() to iterate over the dictionary
+    for name, std_dev in stocks.items():
+        total += std_dev
+
+    for name, std_dev in stocks.items():
+        stock_dict[name] = (std_dev / total) * budget
+        if risk:
+            stock_dict[name] = budget - stock_dict[name]
+        print(name + ": $" + str(stock_dict[name]))
+
+
 def main():
     pkl_path = os.path.join(os.getcwd(), 'adj_list.pkl')
 
     with open(pkl_path, 'rb') as file:
         adj_list = pickle.load(file)
-    
-    budget = input("Budget: $")
-    stock = input("Pick a stock for reference: ")
-    num_stocks = input("How many stocks would you like to invest in?: ")
-    div = input("What would you like to do? (invest or short): ")
-    risk = input("High or low risk investment strategy?: ")
 
+    clean_stck_data = pd.read_csv('../clean_data/stocks_clean.csv')
+    budget = float(input("Budget: $"))
+    stock = input("Pick a stock for reference: ")
+    num_stocks = int(input("How many stocks would you like to invest in?: "))
+    div = input("What would you like to do? (invest or short): ")
+    risk = input("High or low risk investment strategy? (H or L): ")
+    stock_dict = {}
+    risk_bool = False
+    if risk == "H":
+        risk_bool = False
+    if risk == "L":
+        risk_bool = True
     if div == "short":
         stock_heap = MaxHeap(int(num_stocks))
     elif div == "invest":
         stock_heap = MinHeap(int(num_stocks))
-    
+
     for stock, correlation in adj_list[stock].items():
         stock_heap.push((stock, correlation))
-    
-    stock_heap.print_heap()
+
+    for index, row in clean_stck_data.iterrows():
+        for stock_tuple in stock_heap.get_heap():
+            value, name = stock_tuple
+            if row['Name'] == name:
+                stock_name = row['Name']
+                std_dev = row['StandDev']
+                stock_dict[stock_name] = std_dev
+
+
+ #   stock_heap.print_heap()
+
+    amount_to_invest(stock_dict, risk_bool, budget)
+
+
+
 
 # Implements the main
 if __name__ == "__main__":
     main()
-
-
-            
