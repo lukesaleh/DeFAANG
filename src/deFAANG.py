1990fdfba5d433b79063c1842a0fefd9117ea02f
Luke Saleh <lukesaleh@ufl.edu>
2023-12-03 18:07:01 -0500

    More menu input changes and move testing file

--- src/deFAANG.py
+++ src/deFAANG.py
@@ -1,7 +1,6 @@
 import os
-import pickle
-import numpy as np
 import pandas as pd
+import time
 import Knapsack
 import GraphClass
 import Heaps
@@ -29,44 +28,63 @@ def csv_loader(filepath):
     filenames = os.listdir(filepath)
     csv_names = [filename for filename in filenames if filename.endswith('.csv')]
     csv_files = {}
+    #store all data frames in a dictionary where the key is the stock name and value is the df
+    #this allows for easy access
     for name in csv_names:
         df = pd.read_csv(filepath+name)
         csv_files[df.Name.iloc[0]] = df
     return csv_files
 
-def main():
-    # pkl_path = os.path.join(os.getcwd(), 'adj_list.pkl')
+def feature_extraction(csv_files):
+    #extract features to allow easy access of the standard deviation (measure of risk) and latest price of the stock
+    for stock in csv_files.values():
+        std_dev = stock.close.std()
+        name = stock.Name.iloc[0]
+        lastVal = stock.close.iloc[-1]
+        dict = {'Name' : name,'StandDev':std_dev, 'LastClosingVal' : lastVal}
+        ser = pd.Series(dict)
+        new_row_df = pd.DataFrame(ser).transpose()
+        features = pd.concat([features, new_row_df], ignore_index=True)
+    features.to_csv('../clean_data/stocks_clean.csv')
 
-    # with open(pkl_path, 'rb') as file:
-    #     adj_list = pickle.load(file)
+def main():
     start = input("Ready to start the program? (input yes or no) ")
     while(start != "yes"):
         start = input("Ready to start the program? (input yes or no)")
     print('Starting the program!')
+    start_time = time.time()
     print('Importing csv files into dataframes...')
     project_directory = os.path.dirname(os.path.abspath("src"))
     #stocks_directory = os.path.dirname(os.path.abspath(project_directory)) 
     csv_files = csv_loader(project_directory+'/individual_stocks_5yr/')
-    print('Successfully imported all data!')
-    data_structure = input("Would you like to build an adjacency matrix or an adjacency list? (M or L) ")
+    print('Successfully imported all data in ',round(time.time()-start_time, 4),' seconds!\n')
+
+    print('Extracting key features from the data for later analysis...')
+    features = pd.DataFrame(columns = ['Name', 'StandDev', 'LastClosingVal'])
+    start_time = time.time()
+    
+    print('Feature extraction complete in ',round(time.time()-start_time, 4),' seconds!\n')
+    data_structure = input("Would you like to build an adjacency matrix or an adjacency list? (M or L)\n")
     
     if data_structure == 'M':
         print('Building a graph as adjacency matrix...')
         graph = GraphClass.AdjacencyMatrixGraph()
+        start_time = time.time()
         GraphClass.build_graph(csv_files, graph)
-        print('Successfully built an adjacency matrix!')
+        print('Successfully built an adjacency matrix in', round(time.time()-start_time, 4), ' seconds!\n')
     elif data_structure == 'L':
         print('Building a graph as adjacency list...')
         graph = GraphClass.AdjacencyListGraph()
+        start_time = time.time()
         GraphClass.build_graph(csv_files, graph)
-        print('Successfully built an adjacency list!')
+        print('Successfully built an adjacency list in', round(time.time()-start_time, 4), ' seconds!\n')
 
     clean_stck_data = pd.read_csv(project_directory+'/clean_data/stocks_clean.csv')
-    budget = float(input("Budget (No spaces or commas): $"))
-    stock = input("Pick a stock from which you'd like to draw correlations (Ex: AAPL): ")
-    num_stocks = int(input("How many stocks would you like to invest in?: "))
-    div = input("What would you like to do? (invest or short): ")
-    risk = input("High or low risk investment strategy? (H or L): ")
+    budget = float(input("Budget (No spaces or commas):\n$"))
+    stock = input("Pick a stock from which you'd like to draw correlations (Ex: AAPL):\n")
+    num_stocks = int(input("How many stocks would you like to invest in?:\n"))
+    div = input("What would you like to do? (invest or short):\n")
+    risk = input("High or low risk investment strategy? (H or L):\n")
 
     stock_dict = {}
     risk_bool = False
